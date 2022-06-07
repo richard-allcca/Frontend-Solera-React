ffe379e478fc4f55299ed635b24b799e40e11956
richard-allcca <53624835+richard-allcca@users.noreply.github.com>
2022-06-07 11:23:08 -0500

    End Create Read Update Delete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import { useContext, useEffect } from 'react';
-// firebase
-import { deleteDoc, doc, getDoc, getFirestore } from 'firebase/firestore';
 // styles
 import './App.css';
 // context
 import ListCardContext from './context/listCardContext';
-import ServiceContext from './context/serviceContext';
 // components
 import FormService from './components/Form/FormService';
 import Card from './components/Card/Card';
 import Navbar from './components/NavBar/Navbar';
-// conecxion con firebase
-import credentials from './credenciales'
-const db = getFirestore(credentials);
 
 function App() {
 
   const { list, getList } = useContext(ListCardContext)
-  const { setService, serviceIdDb } = useContext(ServiceContext)
 
 
   useEffect(() => {
@@ -25,29 +18,6 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-
-  useEffect(() => {
-    if (serviceIdDb !== '') {
-      console.log('boton de actualizar');
-      getOneUser(serviceIdDb)
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [serviceIdDb])
-
-
-  // TODO: ver donde va esta funcion
-  const getOneUser = async (id) => {
-    try {
-      const docRef = doc(db, `services`, id)
-      const docSnap = await getDoc(docRef)
-      console.log(docSnap.data())
-      // ? pintar los datos en formuario para actualizar
-      setService(docSnap.data())
-
-    } catch (error) {
-      console.log(error)
-    }
-  }
 
   return (
     <div className="App">
@@ -62,7 +32,7 @@ function App() {
 
         <section className="list-cards">
           {list.map(item => {
-            return <Card key={item.id} nombre={item.nombre} descripcion={item.descripcion} />
+            return <Card key={item.id} id={item.id} nombre={item.nombre} descripcion={item.descripcion} />
           })}
         </section>
 
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,41 @@
+import { deleteDoc, doc, getDoc, getFirestore } from 'firebase/firestore';
+import { useContext, useEffect } from 'react';
+import ServiceContext from '../../context/serviceContext';
 import './card.css'
+// coneccion con firebase
+import credentials from '../../credenciales'
+import ListCardContext from '../../context/listCardContext';
+const db = getFirestore(credentials);
 
-const Card = ({ nombre, descripcion }) => {
+const Card = ({ id, nombre, descripcion }) => {
+
+  const { getList } = useContext(ListCardContext)
+  const { setService, serviceIdDb, setServiceIdDb } = useContext(ServiceContext)
+
+  useEffect(() => {
+    if (serviceIdDb !== '') {
+      getOneUser(serviceIdDb)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [serviceIdDb])
 
 
+  const getOneUser = async (id) => {
+    try {
+      const docRef = doc(db, `services`, id)
+      const docSnap = await getDoc(docRef)
+      setService(docSnap.data())
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const deleteService = async (id) => {
+    console.log(id);
+    await deleteDoc(doc(db, `services`, id))
+    getList()
+  }
 
   return (
     <div className='card-content'>
@@ -12,8 +45,8 @@ const Card = ({ nombre, descripcion }) => {
       </div>
 
       <div className="card-footer">
-        <a href="home" className="card-link">Editar</a>
-        <a href="home" className="card-link" >Eliminar</a>
+        <input type="button" href="home" className="card-button" onClick={() => setServiceIdDb(id)} value="Editar" />
+        <input type="button" className="card-button" onClick={() => deleteService(id)} value="Eliminar" />
       </div>
     </div>
 
